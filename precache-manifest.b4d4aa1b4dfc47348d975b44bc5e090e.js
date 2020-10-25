e3d5b08c98423608b15eb374ed148d4af1291457
Lavesh Gaurav <lavesh.g96@gmail.com>
2020-10-25 16:19:22 +0530

    Updates

---
+++ precache-manifest.b4d4aa1b4dfc47348d975b44bc5e090e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99a36002c3e6adcb0c4b602344e281ba",
+    "url": "/bdshop/index.html"
+  },
+  {
+    "revision": "91ab27b4328c86110d59",
+    "url": "/bdshop/static/css/main.fe156e3e.chunk.css"
+  },
+  {
+    "revision": "dad402ee2facbc908d90",
+    "url": "/bdshop/static/js/2.22aeb93b.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/bdshop/static/js/2.22aeb93b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb1ba17bbb897af637f2",
+    "url": "/bdshop/static/js/3.367e1089.chunk.js"
+  },
+  {
+    "revision": "acfd52822c1d331e80fd8113f48d6360",
+    "url": "/bdshop/static/js/3.367e1089.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdf91de18d7793604d42",
+    "url": "/bdshop/static/js/4.63e161c2.chunk.js"
+  },
+  {
+    "revision": "0512cf1466087f5c44c5c61e4b32a001",
+    "url": "/bdshop/static/js/4.63e161c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15ccbe062d65063c37e4",
+    "url": "/bdshop/static/js/5.168e4ef4.chunk.js"
+  },
+  {
+    "revision": "3a7bc1760b9d274e2e5e7531bce86076",
+    "url": "/bdshop/static/js/5.168e4ef4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91ab27b4328c86110d59",
+    "url": "/bdshop/static/js/main.ce406f1a.chunk.js"
+  },
+  {
+    "revision": "e04ad21b06a378b01d1a",
+    "url": "/bdshop/static/js/runtime-main.1954dfb8.js"
+  }
+]);
